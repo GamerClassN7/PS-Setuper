b0b22b7b0a6e0e9d0f31b954d3f67578a730fb72
JonatanRek <gamerclassn7@gmail.com>
2022-04-13 08:48:58 +0200

    Fixes

--- README.md
+++ README.md
@@ -2,13 +2,18 @@
 
 ## Tasks
 1. Backup/Recovery of installed Apps wia WinGet
-1. Backup/Recovery of installed Extensions in VS Code
-1. Backup/Recovery of setting for FileZilla
-
-2. Create new separate partition for data
-3. (Win10) - Disable OneDrive (Optional)
-4. (Win11) - Start to Left
-5. (Win11) - Old context Menu Back 
+2. Backup/Recovery of installed Extensions in VS Code
+3. Backup/Recovery of setting for FileZilla
+4. Backup/Recovery of remembered wifi networks
+5. Create new separate partition for data
+6. (Win10) - Disable OneDrive (Optional)
+7. (Win11) - Start to Left
+8. (Win11) - Old context Menu Back 
+9. (Win11&10) - Dark Mode
+10. (Win11&10) - Show file extensions
 
 ## Feature Plans
 * https://techwiser.com/windows-registry-hacks/
+
+## Thanks
+7zip pasword archive -> https://blog.danskingdom.com/powershell-function-to-create-a-password-protected-zip-file/
--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,7 @@
-
+param(
+    [Parameter(Mandatory = $false)]
+    [Switch]$recovery
+)
 
 #Administrator Permission Level Check
 $isAdministrator = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
@@ -10,70 +13,30 @@ if (-not ($isAdministrator)) {
 #Get Enviroment Variables
 $osName = (Get-ComputerInfo -Property OsName).OsName;
 $userName = $env:UserName;
+$rootBackupPath = "C:\Users\{0}\AppData\Local\Temp\setuperBackup" -f $userName;
+New-Item -Path ("C:\Users\{0}\AppData\Local\Temp\" -f $userName) -Name "setuperBackup" -ItemType "directory" -Force
+
 
 #Menu & Intro
 write-host ("Hello '{0}'" -f $userName)
 write-host ("OS '{0}'" -f $osName)
 
-
-#WinGet
-$hasPackageManager = Get-AppPackage -name "Microsoft.DesktopAppInstaller"
-if (!$hasPackageManager) {
-    $releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $releases = Invoke-RestMethod -uri "$($releases_url)"
-    $latestRelease = $releases.assets | Where-Object { $_.browser_download_url.EndsWith("msixbundle") } | Select-Object -First 1
-    Add-AppxPackage -Path $latestRelease.browser_download_url
-}
-write-host ("Winget Installed")
-    
-$wingetBackupPath = "C:\Users\{0}\AppData\Local\Temp\wgExport.json" -f $userName;
-if (-not [System.IO.File]::Exists($wingetBackupPath)) {
-    winget export -o "$wingetBackupPath" >> $null
-}
-
-$wingetAppList = Get-Content $wingetBackupPath | ConvertFrom-Json
-$wingetAppList.Sources.Packages | ForEach-Object {
-    #TODO: Speed Up Iteration
-    $exist = (winget list $_.PackageIdentifier).Trim()
-    if ($exist -eq "No installed package found matching input criteria.") {
-        write-host $_.PackageIdentifier
-        winget install --id $_.PackageIdentifier
-        return;
-    }
-    write-host ("Already installed '{0}'" -f $_.PackageIdentifier)
-}
-
-#VsCode
-$vsBackupPath = "C:\Users\{0}\AppData\Local\Temp\vsExport.json" -f $userName;
-if (-not [System.IO.File]::Exists($vsBackupPath)) {
-    $commandOutput = & code --list-extensions
-    $commandOutput.Split([Environment]::NewLine) | ConvertTo-Json | Out-File -Encoding utf8 -FilePath $vsBackupPath
-}
-
-$vsExtensionsList = Get-Content $vsBackupPath | ConvertFrom-Json
-$vsExtensionsListActual = & code --list-extensions
-$vsExtensionsList | Where-Object -Filter { -not $vsExtensionsListActual.Contains($_) } | ForEach-Object {
-    write-host $_
-    code --install-extension $_
-}
-
 #Backup FIlezilla Configs
-$fzBackupPath = "C:\Users\{0}\AppData\Local\Temp\fzExport\" -f $userName;
-New-Item -Path ("C:\Users\{0}\AppData\Local\Temp\" -f $userName) -Name "fzExport" -ItemType "directory"
-Copy-Item ("C:\Users\{0}\AppData\Roaming\FileZilla\filezilla.xml" -f $userName) -Destination $fzBackupPath
-Copy-Item ("C:\Users\{0}\AppData\Roaming\FileZilla\layout.xml" -f $userName) -Destination $fzBackupPath
-Copy-Item ("C:\Users\{0}\AppData\Roaming\FileZilla\sitemanager.xml" -f $userName) -Destination $fzBackupPath
+$fzBackupPath = $rootBackupPath +"\fzExport\";
+New-Item -Path $rootBackupPath -Name "fzExport" -ItemType "directory" -Force
+Copy-Item ("C:\Users\{0}\AppData\Roaming\FileZilla\filezilla.xml" -f $userName) -Destination $fzBackupPath -Force
+Copy-Item ("C:\Users\{0}\AppData\Roaming\FileZilla\layout.xml" -f $userName) -Destination $fzBackupPath -Force
+Copy-Item ("C:\Users\{0}\AppData\Roaming\FileZilla\sitemanager.xml" -f $userName) -Destination $fzBackupPath -Force
 
 #Backup Tabby Configs
-$tbBackupPath = "C:\Users\{0}\AppData\Local\Temp\tbExport\" -f $userName;
-New-Item -Path ("C:\Users\{0}\AppData\Local\Temp\" -f $userName) -Name "tbExport" -ItemType "directory"
-Copy-Item ("C:\Users\{0}\AppData\Roaming\tabby\config.yaml" -f $userName) -Destination $tbBackupPath
+$tbBackupPath = $rootBackupPath +"\tbExport\";
+New-Item -Path $rootBackupPath -Name "tbExport" -ItemType "directory" -Force
+Copy-Item ("C:\Users\{0}\AppData\Roaming\tabby\config.yaml" -f $userName) -Destination $tbBackupPath -Force
 
 #Backup KeeWeb Configs
-$tbBackupPath = "C:\Users\{0}\AppData\Roaming\KeeWeb\" -f $userName;
-New-Item -Path ("C:\Users\{0}\AppData\Local\Temp\" -f $userName) -Name "kwExport" -ItemType "directory"
-Copy-Item ("C:\Users\{0}\AppData\Roaming\KeeWeb\app-settings.dat" -f $userName) -Destination $tbBackupPath
+$tbBackupPath = $rootBackupPath +"\kwExport\";
+New-Item -Path $rootBackupPath -Name "kwExport" -ItemType "directory" -Force
+Copy-Item ("C:\Users\{0}\AppData\Roaming\KeeWeb\app-settings.dat" -f $userName) -Destination $tbBackupPath -Force
 
 function New-RegFolder {
     [CmdletBinding()]
@@ -167,6 +130,39 @@ function Set-RegItem {
     Set-ItemProperty @newItem
 }
 
+function Write-ZipUsing7Zip([string]$FilesToZip, [string]$ZipOutputFilePath, [string]$Password, [ValidateSet('7z','zip','gzip','bzip2','tar','iso','udf')][string]$CompressionType = 'zip', [switch]$HideWindow)
+{
+    # Look for the 7zip executable.
+    $pathTo32Bit7Zip = "C:\Program Files (x86)\7-Zip\7z.exe"
+    $pathTo64Bit7Zip = "C:\Program Files\7-Zip\7z.exe"
+    $THIS_SCRIPTS_DIRECTORY = Split-Path $script:MyInvocation.MyCommand.Path
+    $pathToStandAloneExe = Join-Path $THIS_SCRIPTS_DIRECTORY "7za.exe"
+    if (Test-Path $pathTo64Bit7Zip) { $pathTo7ZipExe = $pathTo64Bit7Zip }
+    elseif (Test-Path $pathTo32Bit7Zip) { $pathTo7ZipExe = $pathTo32Bit7Zip }
+    elseif (Test-Path $pathToStandAloneExe) { $pathTo7ZipExe = $pathToStandAloneExe }
+    else { throw "Could not find the 7-zip executable." }
+
+    # Delete the destination zip file if it already exists (i.e. overwrite it).
+    if (Test-Path $ZipOutputFilePath) { Remove-Item $ZipOutputFilePath -Force }
+
+    $windowStyle = "Normal"
+    if ($HideWindow) { $windowStyle = "Hidden" }
+
+    # Create the arguments to use to zip up the files.
+    # Command-line argument syntax can be found at: http://www.dotnetperls.com/7-zip-examples
+    $arguments = "a -t$CompressionType ""$ZipOutputFilePath"" ""$FilesToZip"" -mx9"
+    if (!([string]::IsNullOrEmpty($Password))) { $arguments += " -p$Password" }
+
+    # Zip up the files.
+    $p = Start-Process $pathTo7ZipExe -ArgumentList $arguments -Wait -PassThru -WindowStyle $windowStyle
+
+    # If the files were not zipped successfully.
+    if (!(($p.HasExited -eq $true) -and ($p.ExitCode -eq 0)))
+    {
+        throw "There was a problem creating the zip file '$ZipFilePath'."
+    }
+}
+
 #OS Common
 if ($osName.startsWith("Microsoft Windows 10") -or $osName.startsWith("Microsoft Windows 11")) {
     #Dark-Mode
@@ -178,7 +174,7 @@ if ($osName.startsWith("Microsoft Windows 10") -or $osName.startsWith("Microsoft
 
     #Visueal Efects
     Set-RegItem -ItemPath "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -ItemName "VisualFXSetting" -ItemType "DWORD" -ItemValue 4
-
+}
 
 #OS Specific Tasks
 if ($osName.startsWith("Microsoft Windows 10")) {
@@ -207,4 +203,78 @@ if ($osName.startsWith("Microsoft Windows 11")) {
 # Add disable of Administraor Default #>
 
 
+
+#NEW ITERATION#
+##Backup Wifis##
+$wifiBackupPath = $rootBackupPath +"\wfExport";
+if ($recovery.IsPresent) {
+    Get-ChildItem $wifiBackupPath | ForEach-Object {
+        $profilePath = ($wifiBackupPath + "\" + $_.Name);
+        % { (netsh wlan add profile file="$profilePath" user=current) } 
+    } 
+}
+else {
+    New-Item -Path $rootBackupPath -Name "wfExport" -ItemType "directory" -Force
+    (netsh wlan show profiles) | Select-String "\:(.+)$" | % { $name = $_.Matches.Groups[1].Value.Trim(); $_ } | % { (netsh wlan export profile name="$name" folder="$wifiBackupPath" key=clear) }
+}
+
+##WINGET##
+$hasPackageManager = Get-AppPackage -name "Microsoft.DesktopAppInstaller"
+if (!$hasPackageManager) {
+    $releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $releases = Invoke-RestMethod -uri "$($releases_url)"
+    $latestRelease = $releases.assets | Where-Object { $_.browser_download_url.EndsWith("msixbundle") } | Select-Object -First 1
+    Add-AppxPackage -Path $latestRelease.browser_download_url
+}
+write-host ("Winget Installed")
+    
+$wingetBackupPath = $rootBackupPath +"\wgExport.json";
+if ($recovery.IsPresent) {
+    if (-not [System.IO.File]::Exists($wingetBackupPath)) {
+
+        $wingetAppList = Get-Content $wingetBackupPath | ConvertFrom-Json
+        $wingetAppList.Sources.Packages | ForEach-Object {
+            #TODO: Speed Up Iteration
+            $exist = (winget list $_.PackageIdentifier).Trim()
+            if ($exist -eq "No installed package found matching input criteria.") {
+                write-host $_.PackageIdentifier
+                winget install --id $_.PackageIdentifier
+                return;
+            }
+            write-host ("Already installed '{0}'" -f $_.PackageIdentifier)
+        }
+    }
+    else {
+        write-host ("no VS code backup file found")
+    }
+}
+else {
+    winget export -o "$wingetBackupPath" >> $null
+}
+
+##VS CODE##
+$vsBackupPath = $rootBackupPath +"\vsExport.json";
+if ($recovery.IsPresent) {
+    if (-not [System.IO.File]::Exists($vsBackupPath)) {
+        $vsExtensionsList = Get-Content $vsBackupPath | ConvertFrom-Json
+        $vsExtensionsListActual = & code --list-extensions
+        $vsExtensionsList | Where-Object -Filter { -not $vsExtensionsListActual.Contains($_) } | ForEach-Object {
+            write-host $_
+            code --install-extension $_
+        }
+    }
+    else {
+        write-host ("no VS code backup file found")
+    }
+}
+else {
+    $commandOutput = & code --list-extensions
+    $commandOutput.Split([Environment]::NewLine) | ConvertTo-Json | Out-File -Encoding utf8 -FilePath $vsBackupPath
+}
+
+
+Write-ZipUsing7Zip -FilesToZip $rootBackupPath -ZipOutputFilePath ("C:\Users\{0}\Desktop\SetuperBackup.zip" -f $userName) -Password "password123"
+
 Write-Host "DONE :)"
+
